f9c23520e9087b2ced7aeed59d160f66d8b26234
Chi Anh Bui <chrisbui.dev188@gmail.com>
2024-05-17 01:07:15 -0400

    v2: clean up

--- src/components/BorderTabs.jsx
+++ src/components/BorderTabs.jsx
@@ -7,7 +7,7 @@ const BorderTabs = ({ onTabClick }) => {
   return (
     <div className="justify-self-center flex flex-col gap-4 items-center ">
       <p>Change border styles</p>
-      <menu className="flex p-2 bg-zinc-900 border border-zinc-800  rounded-md">
+      <menu className="flex p-2 bg-zinc-800 border border-zinc-700  rounded-md">
         {['dashed', 'dotted', 'solid'].map((item) => (
           <button
             className={`px-3 py-1 md:px-4 md:py-2 rounded-md capitalize ${
--- src/data/index.js
+++ src/data/index.js
@@ -26,4 +26,13 @@ export const projects = [
     githubUrl: 'fake github url',
     liveUrl: 'fake live url',
   },
+  {
+    id: crypto.randomUUID(),
+    name: 'Personal Portfolio',
+    description:
+      "The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from 'de Finibus Bonorum et Malorum' by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.",
+    imageUrl: '/images/personal-website.png',
+    githubUrl: 'fake github url',
+    liveUrl: 'fake live url',
+  },
 ]
